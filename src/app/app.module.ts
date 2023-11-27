51c02279b04d67a7a3ed528808a65e35ed4cc82f
MorganeLorcery <morgane.lorcery@etu.unice.fr>
2023-11-27 17:12:11 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PositionComponent } from './shared/components/position/position.component';
+import { TableComponent } from './shared/components/table/table.component';
+import { DishComponent } from './shared/components/dish/dish.component';
+import { SideTableComponent } from './shared/components/side-table/side-table.component';
+import { RecipeComponent } from './shared/components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionComponent,
+    TableComponent,
+    DishComponent,
+    SideTableComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
