ef3fe5d4f4e02e1f0ea2819f67f42d35ef5b7ff8
Baptiste-Chatelain <70631836+Baptiste-Chatelain@users.noreply.github.com>
2023-12-04 09:59:05 +0100

    adaptation affichage taille

--- src/app/pages/mural/normal-mode/normal-mode.component.ts
+++ src/app/pages/mural/normal-mode/normal-mode.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TableService} from "../../../services/table.service";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { TableService } from "../../../services/table.service";
 import { TableComponent } from "../../../shared/components/table/table.component";
 
 @Component({
@@ -9,12 +10,22 @@ import { TableComponent } from "../../../shared/components/table/table.component
 })
 export class NormalModeComponent implements OnInit {
   tables: TableComponent[] = [];
+  isTabletMode = false;
 
-  constructor(private tableService: TableService) {}
+  constructor(
+    private tableService: TableService,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
     this.tableService.getTables().subscribe(receivedTables => {
       this.tables = receivedTables;
     });
+
+    // Detect changes in screen size to determine tablet mode
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(result => {
+        this.isTabletMode = result.matches;
+      });
   }
 }
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,15 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DishComponent} from "../dish/dish.component";
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { DishComponent } from '../dish/dish.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
   @Input() numberTable!: number;
   @Input() numberOrder!: number;
   @Input() dishes!: DishComponent[];
   @Input() date!: Date;
 
+  @Output() tableClick = new EventEmitter<void>();
+
+  isTabletMode = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    // Détectez les changements de taille d'écran pour déterminer le mode tablette
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(result => {
+        this.isTabletMode = result.matches;
+      });
+  }
+
+  handleTableClick() {
+    if (this.isTabletMode) {
+      console.log('Table clicked in tablet mode');
+      this.tableClick.emit();
+    }
+  }
+
+  getDisplayedDishes(): DishComponent[] {
+    // Seulement les 4 premiers plats en mode tablette
+    return this.isTabletMode ? this.dishes.slice(0, 4) : this.dishes;
+  }
 }
